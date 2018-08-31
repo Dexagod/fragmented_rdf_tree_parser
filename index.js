1ea4fa2846b1506160929d083565c0947f41ad52
Amory Hoste <amory.hoste@ugent.be>
2018-08-31 16:12:34 +0200

    add

--- index.js
+++ index.js
@@ -1,88 +1,92 @@
-let ldfetch = require('ldfetch');
+const ldfetch = require('ldfetch');
+const LRU = require("lru-cache");
 
-const fetch = new ldfetch({});
+// TODO: childrelations can be on another webpage
 
-module.exports.parseTree = function(url, singles = new Set()) {
+class TreeParser {
 
-    return fetch.get(url).then((result) => {
-        return parse(result.triples, singles);
-    });
-};
-
-
-function parse(triples, singles) {
-    let nodes = {};
-    let collections = {};
-    let unidentified = {};
-    let listItems = {};
-
-    triples.forEach((triple) => {
-        let subject = triple.subject.value;
-        let predicate = triple.predicate.value;
-        let object = triple.object.value;
-
-        if (object.slice(0, 2) === "_:") {
-            let objectCopy = object;
-            if (listItems.hasOwnProperty(objectCopy)) {
-                object = listItems[objectCopy];
-            } else {
-                object = {};
-                listItems[objectCopy] = object;
-            }
-        }
-
-        if (predicate === "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" && object === "https://w3id.org/tree#Node") {
-            addTriple(nodes, subject, predicate, object, singles.has(predicate));
-
-            // Copy unidentified data to node
-            let toAdd = unidentified[subject];
-            if (toAdd !== undefined) {
-                nodes[subject] = Object.assign(nodes[subject], toAdd);
-                delete unidentified[subject];
-            }
-        } else if (predicate === "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" && object === "http://www.w3.org/ns/hydra/core#Collection") {
-            addTriple(collections, subject, predicate, object, singles.has(predicate));
-
-            // Copy unidentified data to node
-            let toAdd = unidentified[subject];
-            if (toAdd !== undefined) {
-                collections[subject] = Object.assign(collections[subject], toAdd);
-                delete unidentified[subject];
-            }
-        } else if (subject.slice(0, 2) === "_:") {
-            addTriple(listItems, subject, predicate, object, singles.has(predicate));
+    constructor(fetch) {
+        if (fetch === undefined) {
+            this.fetch = new ldfetch({});
         } else {
-            if (nodes.hasOwnProperty(subject)) {
-                addTriple(nodes, subject, predicate, object, singles.has(predicate));
-            } else if (collections.hasOwnProperty(subject)) {
-                addTriple(collections, subject, predicate, object, singles.has(predicate));
-            } else{
-                addTriple(unidentified, subject, predicate, object, singles.has(predicate));
+            this.fetch = fetch;
+        }
+    }
+
+    parseTree(url, singles = new Set()) {
+        return this.fetch.get(url).then((result) => {
+            return this._parse(result.triples, singles);
+        });
+    }
+
+    async _parse(triples, singles) {
+        let nodes = {};
+        let collections = {};
+        let members = {};
+        let childrelations = {};
+
+        let unidentified = {};
+        let listItems = {};
+
+        for (let i = 0; i < triples.length; i++) {
+            let triple = triples[i];
+            let subject = triple.subject.value;
+            let predicate = triple.predicate.value;
+            let object = triple.object.value;
+
+            // Fix list items
+            if (object.slice(0, 2) === "_:") {
+                let objectCopy = object;
+                if (listItems.hasOwnProperty(objectCopy)) {
+                    object = listItems[objectCopy];
+                } else {
+                    object = {};
+                    listItems[objectCopy] = object;
+                }
+            }
+
+            if (predicate === "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" && object === "https://w3id.org/tree#Node") {
+                this._addTriple(nodes, subject, predicate, object, singles.has(predicate));
+                copyOver(unidentified, nodes, subject);
+            } else if (predicate === "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" && object === "http://www.w3.org/ns/hydra/core#Collection") {
+                this._addTriple(collections, subject, predicate, object, singles.has(predicate));
+                copyOver(unidentified, nodes, subject);
+            } else if (subject.slice(0, 2) === "_:") {
+                this._addTriple(listItems, subject, predicate, object, singles.has(predicate));
+            } else {
+                if (nodes.hasOwnProperty(subject)) {
+                    this._addTriple(nodes, subject, predicate, object, singles.has(predicate));
+                } else if (collections.hasOwnProperty(subject)) {
+                    this._addTriple(collections, subject, predicate, object, singles.has(predicate));
+                } else {
+                    this._addTriple(unidentified, subject, predicate, object, singles.has(predicate));
+                }
             }
         }
-    });
 
-    removeEmptyNodes(nodes);
-    return {
-        nodes: nodes,
-        collections: collections
-    };
-}
-
-function addTriple(obj, subject, predicate, object, single) {
-    if (!obj.hasOwnProperty(subject)) {
-        obj[subject] = {};
+        removeEmptyNodes(nodes);
+        return {
+            nodes: nodes,
+            collections: collections
+        };
     }
 
-    let sub = obj[subject];
-    if (single) {
-        sub[predicate] = object;
-    } else {
-        if (! sub.hasOwnProperty(predicate)) {
-            sub[predicate] = []
+    _addTriple(obj, subject, predicate, object, single) {
+        if (!obj.hasOwnProperty(subject)) {
+            obj[subject] = {};
         }
-        sub[predicate].push(object);
+
+        let sub = obj[subject];
+        if (single) {
+            sub[predicate] = object;
+        } else {
+            if (! sub.hasOwnProperty(predicate)) {
+                sub[predicate] = []
+            }
+            sub[predicate].push(object);
+        }
     }
+
 }
 
 function removeEmptyNodes(nodes) {
@@ -94,3 +98,14 @@ function removeEmptyNodes(nodes) {
         }
     }
 }
+
+function copyOver(from, to, subject) {
+    // Copy unidentified data
+    let toAdd = from[subject];
+    if (toAdd !== undefined) {
+        to[subject] = Object.assign(to[subject], toAdd);
+        delete from[subject];
+    }
+}
+
+module.exports = TreeParser;
